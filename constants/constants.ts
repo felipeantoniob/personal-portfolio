6effa14394ed54b72acc67028904ca63e6a978eb
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2022-07-03 12:16:21 +0800

    Fix image url

--- constants/constants.ts
+++ constants/constants.ts
@@ -3,7 +3,7 @@ export const projects = [
     title: 'Spotifaves',
     description:
       ' A website where users can view their top artists, top tracks, top genres, and recently played tracks on Spotify. They can also generate playlists based on their personalized data to share with friends.',
-    image: '/../public/images/spotifaves.png',
+    image: '/images/spotifaves.png',
     tags: ['react', 'javascript'],
     codeUrl: 'https://github.com/felipeantoniob/spotifaves#readme',
     siteUrl: 'https://spotifaves.vercel.app/',
@@ -14,7 +14,7 @@ export const projects = [
     title: 'Cinehub',
     description:
       ' A website that lets users search for movies and view its details such as the plot summary, genres, and actors. It queries the TMDB API to retrieve movies based on a search term.',
-    image: '/../public/images/cinehub.png',
+    image: '/images/cinehub.png',
     tags: ['react', 'javascript'],
     codeUrl: 'https://github.com/felipeantoniob/cinehub#readme',
     siteUrl: 'https://cinehub.vercel.app',
@@ -25,7 +25,7 @@ export const projects = [
     title: 'JS-808',
     description:
       'An interactive drum machine website where users can play 16 drum pads with their mouse or keyboard. The drum samples were sampled from a Roland TR-808 drum machine.',
-    image: '/../public/images/js_808.png',
+    image: '/images/js_808.png',
     tags: ['react', 'javascript'],
     codeUrl: 'https://github.com/felipeantoniob/JS-808#readme',
     siteUrl: 'https://js-808.vercel.app/',
@@ -36,7 +36,7 @@ export const projects = [
     title: 'To-Do List',
     description:
       'A simple to-do list website where users can add items to the list, mark items as complete or incomplete, edit items, and delete items.',
-    image: '/../public/images/todo_list.png',
+    image: '/images/todo_list.png',
     tags: ['react', 'javascript'],
     codeUrl: 'https://github.com/felipeantoniob/todo-list#readme',
     siteUrl: 'https://next-todo-app-two.vercel.app',
@@ -48,7 +48,7 @@ export const projects = [
     title: 'Quiz Game',
     description:
       'A quiz website where users can select a category, difficulty, and number of questions. Questions are fetched from Open Trivia Database, a free API with thousands of user-contributed trivia questions',
-    image: '/../public/images/quiz_game.png',
+    image: '/images/quiz_game.png',
     tags: ['react', 'javascript'],
     codeUrl: 'https://github.com/felipeantoniob/next-quiz-app#readme',
     siteUrl: 'https://next-quiz-app.vercel.app',
